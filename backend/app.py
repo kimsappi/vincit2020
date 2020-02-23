10d41bd9c1eda254c1f835720139411ae178ea02
kimsappi <sappikim@gmail.com>
2020-02-23 08:50:45 -0800

    Cleaned up files, added Dockerfile, final changes for deployment

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, jsonify, request, render_template
 
 import config
@@ -71,4 +73,4 @@ def reset_points():
 
 
 if __name__ == "__main__":
-	app.run(host="0.0.0.0", debug=True)
+	app.run(host="0.0.0.0", port=5000)
--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cd frontend
-npm run build
-cd ..
-python3 backend/app.py
