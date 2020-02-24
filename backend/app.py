f14fc21a77db6ece8d6d2f7ba31f4c838b667fb8
kimsappi <sappikim@gmail.com>
2020-02-23 17:43:37 -0800

    Listening to $PORT

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, jsonify, request, render_template
 
 import config
@@ -77,4 +79,4 @@ def reset_points():
 
 
 if __name__ == "__main__":
-	app.run(host="0.0.0.0", port=5000)
+	app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
